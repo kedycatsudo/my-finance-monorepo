f3875ad652de71500b692ea6865ed4a65dbb3093
Your Name <dkocausta34@gmail.com>
2026-02-16 20:50:55 -0500

    backend auth service updated to match with frontend api call

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -19,6 +19,6 @@ export class AuthService {
   }
   async login(user: any) {
     const payload = { username: user.username, sub: user.id };
-    return { access_token: this.jwtService.sign(payload) };
+    return { access_token: this.jwtService.sign(payload), user };
   }
 }
